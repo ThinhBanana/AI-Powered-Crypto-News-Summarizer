74cbea146287e5aa8c142297224a219d5c367ad9
MT-059\ThinhTT1 <thinhtt1@hblab.vn>
2025-06-17 17:38:27 +0700

    feat: analyze sentiment to know news is positive or negative

---
+++ src/analyze_sentiment.py
@@ -0,0 +1,14 @@
+from transformers import pipeline
+
+sentiment_analyzer = pipeline("sentiment-analysis")
+
+def analyze(text: str) -> dict:
+    if len(text.split()) < 10:
+        return {'label': 'NEUTRAL', 'score': 0.0}
+
+    output = sentiment_analyzer(text)
+    return output[0]
+
+print(analyze("Trump pocketed more than $57 million from token sales by the crypto venture he and his sons helped launch last year, according to federal financial disclosure forms released by the White House."))
+print(analyze("Bitcoin reached a new all-time high, exciting investors worldwide."))
+print(analyze("Massive hacks have made the crypto market unstable and unreliable."))
